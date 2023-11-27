a0711de72df4a6b110c9923d3d62ac41a0dca8bf
Pratyusha <111060867+adminpratyusha@users.noreply.github.com>
2023-11-27 11:35:31 +0530

    Update maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,3 +1,10 @@
 def build(){
                 sh 'mvn clean install -DskipTests'
       }
+def unittest(){
+                  sh 'mvn test'
+}
+def integrationtest(){
+                sh 'mvn verify -DskipUnitTests'
+
+}
