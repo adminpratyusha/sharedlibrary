7f55b24dd2c77f17fa29c60fcb8843bb1186bbc2
Pratyusha <111060867+adminpratyusha@users.noreply.github.com>
2023-11-28 15:24:44 +0530

    Update nexusrepo.groovy

--- vars/nexusrepo.groovy
+++ vars/nexusrepo.groovy
@@ -3,7 +3,7 @@
                     filesByGlob = findFiles(glob: "target/*.${pom.packaging}");
                     echo "${filesByGlob[0].name} ${filesByGlob[0].path} ${filesByGlob[0].directory} ${filesByGlob[0].length} ${filesByGlob[0].lastModified}"
                     artifactPath = filesByGlob[0].path;
-	            echo "${artifactpath}"
+	            echo "${artifactPath}"
                     artifactExists = fileExists artifactPath;
                     if(artifactExists) {
                         echo "*** File: ${artifactPath}, group: ${pom.groupId}, packaging: ${pom.packaging}, version ${pom.version}";
