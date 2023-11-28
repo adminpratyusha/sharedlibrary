951fa9fd93eb7af85c1fb3b091a0d7ee6021bb1f
Pratyusha <111060867+adminpratyusha@users.noreply.github.com>
2023-11-28 10:51:17 +0530

    Update docker.groovy

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -1,9 +1,9 @@
-def docker(String IMAGE_NAME,String BUILD_ID,String dockercred){
+def docker(String IMAGE_NAME,String BUILD_ID,String DOCKER_CREDENTIALS_ID){
                     // Assuming your Dockerfile is in the root directory of your project
                     def dockerImage = docker.build("${IMAGE_NAME}:${BUILD_ID}")
 
                     
-                    docker.withRegistry('https://registry.hub.docker.com', dockercred) {
+                    docker.withRegistry('https://registry.hub.docker.com', DOCKER_CREDENTIALS_ID) {
                         dockerImage.push()
 
 
