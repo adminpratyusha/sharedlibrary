713d8f05e102fb8eb8084af7cf95d56590f6ce4d
Pratyusha <111060867+adminpratyusha@users.noreply.github.com>
2023-11-28 10:33:49 +0530

    Create docker.groovy

---
+++ vars/docker.groovy
@@ -0,0 +1,15 @@
+def docker(String IMAGE_NAME,String BUILD_ID,String dockercred){
+                    // Assuming your Dockerfile is in the root directory of your project
+                    def dockerImage = docker.build("${IMAGE_NAME}:${BUILD_ID}")
+
+                    
+                    docker.withRegistry('https://registry.hub.docker.com', 'dockercred') {
+                        dockerImage.push()
+
+
+                    }
+
+
+
+  
+}
