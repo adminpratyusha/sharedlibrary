b4c0dd59ef68714bcd9fbc17ce72c54ee69c1fca
Pratyusha <111060867+adminpratyusha@users.noreply.github.com>
2023-11-28 10:57:17 +0530

    Update docker.groovy

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -1,4 +1,4 @@
-def docker(String IMAGE_NAME,String BUILD_ID,String DOCKER_CREDENTIALS_ID){
+def dockertask(String IMAGE_NAME,String BUILD_ID,String DOCKER_CREDENTIALS_ID){
                     // Assuming your Dockerfile is in the root directory of your project
                     def dockerImage = docker.build("${IMAGE_NAME}:${BUILD_ID}")
 
