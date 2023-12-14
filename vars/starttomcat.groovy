a69f5b063b3485cec7116704c6fdde828ab4dbc0
Pratyusha <111060867+adminpratyusha@users.noreply.github.com>
2023-12-14 14:00:36 +0530

    Update starttomcat.groovy

--- vars/starttomcat.groovy
+++ vars/starttomcat.groovy
@@ -3,7 +3,7 @@ def start(String SSHCONFIGNAME)
               
                     sshPublisher(publishers: [sshPublisherDesc(configName: SSHCONFIGNAME, transfers: [
                                     sshTransfer(
-                                        execCommand: "sudo cp -rf /home/ubuntu/* /var/lib/tomcat9/webapps && rm -rf /home/ubuntu/* && sudo systemctl start tomcat9",
+                                        execCommand: "sudo cp -rf /home/ubuntu/* /var/lib/tomcat9/webapps && rm -rf /home/ubuntu/* && sudo systemctl restart tomcat9",
                                         execTimeout: 120000
                                     )
                                 ])
